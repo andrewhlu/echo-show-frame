8a7a6696e38a66ad25eccdee85f009db64c06e34
andrewhlu <alu@ucsb.edu>
2022-06-19 00:39:03 -0700

    Modified frame for tablet

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ var captionText = document.getElementById('caption');
 var settingsMenu = document.getElementById("settingsMenu");
 var blackout = document.getElementById("blackout");
 var audioElement = document.getElementById("musicElement");
+var ipDisplayElement = document.getElementById("ip-display");
+
+var addrs = {
+    "0.0.0.0": false
+};
 
 // Functions to control state of frame
 var frameConfig = {
@@ -155,3 +160,57 @@ function hideFrame() {
 function showFrame() {
     blackout.hidden = true;
 }
+
+function showDeviceIP() {
+    if (!!ipDisplayElement) {
+        if (Object.keys(addrs).filter((a) => addrs[a]).length > 0) {
+            ipDisplayElement.innerHTML = Object.keys(addrs).filter((a) => addrs[a])[0];
+        } else {
+            ipDisplayElement.innerHTML = "Could not get device internal IP address";
+        }
+    }
+}
+
+// NOTE: window.RTCPeerConnection is "not a constructor" in FF22/23
+var RTCPeerConnection = /*window.RTCPeerConnection ||*/ window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
+
+if (RTCPeerConnection) (function () {
+    var rtc = new RTCPeerConnection({iceServers:[]});
+    if (1 || window.mozRTCPeerConnection) {      // FF [and now Chrome!] needs a channel/stream to proceed
+        rtc.createDataChannel('', {reliable:false});
+    };
+    
+    rtc.onicecandidate = function (evt) {
+        // convert the candidate to SDP so we can run it through our general parser
+        // see https://twitter.com/lancestout/status/525796175425720320 for details
+        if (evt.candidate) grepSDP("a="+evt.candidate.candidate);
+    };
+    rtc.createOffer(function (offerDesc) {
+        grepSDP(offerDesc.sdp);
+        rtc.setLocalDescription(offerDesc);
+    }, function (e) { console.warn("offer failed", e); });
+
+    function updateDisplay(newAddr) {
+        console.log("updateDisplay called with addr", newAddr);
+        if (newAddr in addrs) return;
+        else if (!newAddr.match(/(?:[0-9]{1,3}\.){3}[0-9]{1,3}/g)) return;
+        else addrs[newAddr] = true;
+        // console.log(Object.keys(addrs).filter((a) => addrs[a]));
+        showDeviceIP();
+    }
+    
+    function grepSDP(sdp) {
+        sdp.split('\r\n').forEach(function (line) { // c.f. http://tools.ietf.org/html/rfc4566#page-39
+            if (~line.indexOf("a=candidate")) {     // http://tools.ietf.org/html/rfc4566#section-5.13
+                var parts = line.split(' '),        // http://tools.ietf.org/html/rfc5245#section-15.1
+                    addr = parts[4],
+                    type = parts[7];
+                if (type === 'host') updateDisplay(addr);
+            } else if (~line.indexOf("c=")) {       // http://tools.ietf.org/html/rfc4566#section-5.7
+                var parts = line.split(' '),
+                    addr = parts[2];
+                updateDisplay(addr);
+            }
+        });
+    }
+})()
